c0c9002bfd4d9beb3ba5515ae80e4ed381e5fcbb
Dobi Tamas <dobi.tamas0428@gmail.com>
2021-02-11 16:45:06 +0100

    Implemented writing to file in UserStory3.py

---
+++ UserStory3.py
@@ -0,0 +1,54 @@
+import UserStory2
+import UserStory1
+
+
+def check_number_for_error(number):
+    if "ERROR" in number:
+        return True
+
+    return False
+
+
+def check_if_valid(number):
+    if UserStory2.checksum(number):
+        return True
+    return False
+
+
+def replace_error(number):
+    for i in range(len(number)):
+        if number[i] == "ERROR":
+            number[i] = "?"
+
+    return number
+
+
+def check_number(number):
+    if check_number_for_error(number):
+        number = replace_error(number)
+        return str(number) + " ILL"
+
+    if check_if_valid(number):
+        return str(number)
+
+    return str(number) + " ERR"
+
+
+def check_all_numbers():
+    numbers = UserStory1.read_entries()
+    lines = []
+    for number in numbers:
+        lines.append(check_number(number))
+
+    return lines
+
+
+def write_to_file():
+    lines = check_all_numbers()
+    output = open('output.txt', "a+")
+
+    for line in lines:
+        output.write(line + "\r\n")
+
+
+write_to_file()
