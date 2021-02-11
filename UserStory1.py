c67f8cc904d280dfadb3b0c3d3948c1889cf31eb
Dobi Tamas <dobi.tamas0428@gmail.com>
2021-02-11 15:18:25 +0100

    Implemented reading lines and to convert digits to numbers

--- UserStory1.py
+++ UserStory1.py
@@ -4,8 +4,23 @@ import Characters
 def read_from_file():
     input_file = open('input.txt')
     lines = input_file.readlines()
-
     return lines
 
 
+def split_lines():
+    numbers = []
 
+    lines = read_from_file()
+
+    for index in range(0, 27, 3):
+        number = [lines[0][index:index+3],
+                  lines[1][index:index+3],
+                  lines[2][index:index+3]]
+        numbers.append(Characters.get_str_from_digit(number))
+
+    return numbers
+
+
+numbers = split_lines()
+
+print(numbers)
